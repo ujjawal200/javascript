ca5735de4458e8ebbc6d0a71727446a64495a713
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-19 07:40:43 +0000

    conversence of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+console.log(typeof score );
+console.log(typeof(score));
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
+
+// conversion is very bad in javascript 
+// "33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+
+//for boolean 
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "abc" => true;
+
+
+let someNumber = 100
+let stringNumber  = String(someNumber)
+console.log(stringNumber );
+console.log(typeof stringNumber);
