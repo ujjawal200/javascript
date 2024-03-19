fe15cda7fb113567e45e1f633541a5aee3d921bd
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-19 07:01:00 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // treat all js code as newer version
+
+// alert(3+3)   // we are using nodejs, not browser
+console.log(3+3) // code readABLE Should be high
+
+
+
+//documentation mdn not original 
+// ecma script original script
+console.log("hitesh") 
+let name = " ujjawal"
+let age = 18
+let isLoggedIn = false
+
+// number  => 2 to power 53
+//big int
+//string  => " "
+// null  => standalone value
+// boolean => true,false
+// undefined => 
+//symbol => for uniqueness 
+
+
+// object
+console.log(typeof undefined); 
+
+// null type is object      **
+// undefined type is undefined 
