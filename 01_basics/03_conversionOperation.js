0609df2cff3de2b5b9487d35a6e6ae8ce2804312
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-20 05:38:23 +0000

    primitve and incremental concept

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,18 @@
 let score = "33abc"
 
-console.log(typeof score );
-console.log(typeof(score));
+//console.log(typeof score );
+//console.log(typeof(score));
 
 let valueInNumber = Number (score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 
 
 
-// conversion is very bad in javascript 
+// conversion is very bad in javascript use typescript
+
 // "33" => 33
 //"33abc" => NaN
 //true => 1
@@ -27,5 +28,82 @@ console.log(valueInNumber);
 
 let someNumber = 100
 let stringNumber  = String(someNumber)
-console.log(stringNumber );
-console.log(typeof stringNumber);
+//console.log(stringNumber );
+//console.log(typeof stringNumber);
+
+
+
+
+
+//********************************************* operations ********************************
+
+ let value =3
+ let negValue = -value;
+ //console.log(negValue);
+
+ //console.log(3**2);        // 3 raise to the power of 2  
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2%3);
+ //console.log(2/3);
+
+
+ let str1 = " hello"
+ let str2 = "hitesh"
+
+ let str3 = str1 + str2 ;
+ //console.log(str3);
+
+
+// from left to right           *********
+//  console.log("1"+ 2);
+//  console.log("1" + "2");
+//  console.log(2 + "2");
+//  console.log(2+3 + "3");
+//  console.log("3" + 2+3);      
+// not good approach use parenthesis/       
+
+// multiselect comment ctrl + /      **********
+
+ // day 2 
+ // ecma scirpt toprimitve   quick read
+
+
+//  console.log( true);
+//  console.log(+true);
+//  console.log(-true);
+//  console.log(-false);
+console.log(true+true);
+console.log(+"");
+console.log(true);
+
+let num1,num2,nums3
+num1 = num2 =num2 = 2+2
+
+
+// incremental operators read from prefix and postfix js mdn ***********
+
+let gameCounter = 100
+gameCounter++;   //postfix
+
+console.log(gameCounter);
+++gameCounter;     // prefix
+console.log(gameCounter);
+
+
+// link to study 
+//https://developer.mozilla.org/en-US/docs/Glossary/Abstraction
+
+ 
+
+
+
+
+
+ 
+
+  
+
+
+  
+
