5924f20d2bc2dde313d8c885410a4d287c6fee02
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-20 06:46:58 +0000

    comparsions

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,35 @@
+// console.log(2 > 1);  // true
+// console.log(2 >= 1);  // true
+// console.log(2 < 5);   // true
+// console.log(2 < 1);   // false
+// console.log(2 == 1);  // false
+// console.log(2 == 2);  // true
+// console.log(2 != 1);  // true
+// console.log(2 != 2);  // false
+
+
+//  console.log("1" > 1);  // ascvii value comparsion
+//  console.log("02" > 1);
+console.log(null > 0);
+console.log(null == 0);     // works diif in javascript
+console.log(null >= 0);   //comparsions convert null to 0   
+
+
+// the teason is that an equality check == and comparsions > < >= <= work differently.
+// comprasions convert null to a number , treating it as 0.
+// that's why 3rd console null >= 0 is true and ist console null >0 is false;
+
+ console.log(undefined == 0);
+ // undefined always give false value
+
+ // === strict check ****************
+ 
+ console.log("2" === 2);
+
+
+
+
+
+
+
+
