11c6851c66a997e3270dbd263dad25c6d4268881
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-29 10:21:10 +0000

    string methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,7 +6,7 @@ console.log(name + repoCount+ " value");
 //string interpolation  === `` placeholder 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('ujjawal');
+const gameName = new String('ujjawal-Thakur');
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -17,3 +17,25 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(5));
 console.log(gameName.indexOf('l'));
 
+
+ const anotherString = gameName.slice(-5,4);     // we can use -ve number too 
+ console.log(anotherString);
+
+ const newStringOne = "           ujjawal           ";
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url = " https://ujjawal.com/ujjawal%20thakur"; 
+
+ console.log((url.replace('%20','_')));     // used to rmove any specific ele from string
+
+ console.log(url.includes('sundar'));        // checks whether it exists in the string or not 
+
+ console.log(gameName.split('-'));
+
+ 
+
+   
+
+  
+ 
