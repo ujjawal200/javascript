dd84222903fdd8a470ed5887984d1070ad5b4ce4
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-03-29 07:17:32 +0000

    string commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = " hitesh";
+const repoCount = 5;
+
+console.log(name + repoCount+ " value");
+
+//string interpolation  === `` placeholder 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ujjawal');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('l'));
+
