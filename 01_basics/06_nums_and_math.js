146a6ab3802209ef17039c2552c833313b5d7a6d
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-05-29 11:46:04 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -24,8 +24,8 @@ console.log(Math);
 
 console.log(Math.abs(-4));
 console.log(Math.round(4.6));
-console.log(Math.ceil(4.6));  // lower value
-console.log(Math.floor(4.6)); // upper value
+console.log(Math.ceil(4.6));  // upper value
+console.log(Math.floor(4.6)); // lower value
 console.log(Math.min(4,5,6,7,8,9,32,1));
 console.log(Math.max(4,5,6,7,8,9,32,1));
 console.log(Math.random());
