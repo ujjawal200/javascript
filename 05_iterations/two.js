c95bf7f01a81bcfbaed4644ae94714d468b02347
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-05-31 10:29:12 +0000

    while do while loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// while and do while loop
+
+let i = 0;
+
+while (i <=2) {
+    console.log(i);
+    i++;    
+}
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score ++;
+} while (score <= 1);
