b2b981a86a7442a3c840f3734f07fa8e768f3c45
UJJAWAL THAKUR <106134726+ujjawal200@users.noreply.github.com>
2024-05-31 10:28:55 +0000

    for loop in js

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+// for loop
+// for (let index = 0; index < 10; index++) {
+//     const element = index ;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// } 
+
+
+// for( let i =  0 ; i < 10 ; i++){
+//     console.log(`outer loop value is : ${i}`);
+//     for( let j = 0 ; j < 10 ;j++){
+//         console.log(`inner loop value is : ${j}`);
+//     }
+// }
+
+// break and continue
+
+for(let i = 1 ; i <= 10 ; i++){
+    if(i == 5){
+        console.log(`5  detected`);
+        break;
+    }
+    console.log(`value of i os ${i}`);
+}
+
+//continue skips and continue
+
+
+ 
+
